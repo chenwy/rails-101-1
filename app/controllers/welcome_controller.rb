5f86d481fb4bf77c8839a46bb2156c9a82386e63
chenwy <chenwy323@163.com>
2016-12-09 19:39:09 +0800

    add helper message

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "Good mornig! Hello!"
+    flash[:alert] = "Good night! Go to sleep!"
+    flash[:warning] = "This is warning message!"
+  end
 end
