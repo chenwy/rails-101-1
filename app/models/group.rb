79600e3e90554055d2e3d257b2c5e67aa1315999
chenwy <chenwy323@163.com>
2016-12-10 19:58:36 +0800

    implement is_member_of?(group)

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,4 +6,8 @@ class Group < ApplicationRecord
   has_many :group_relationships
   has_many :members, through: :group_relationships, source: :user
 
+  def is_member_of?(group)
+    participated_groups.include?(group)
+  end
+
 end
